9ad0b59c7a8778310be30e1fb9070b1c76c42a36
fomin-stas <fomin.stanislav@gmail.com>
2014-09-04 08:48:15 +0400

    db_init spher

---
+++ application/data/db/spher.sql
@@ -0,0 +1,44 @@
+INSERT INTO "spher" VALUES (1, 'Безопасность и охрана правопорядка');
+INSERT INTO "spher" VALUES (2, 'Государственные учреждения
+');
+INSERT INTO "spher" VALUES (3, 'Гражданство, регистрация, визы
+');
+INSERT INTO "spher" VALUES (4, 'Здравоохранение и медицина
+');
+INSERT INTO "spher" VALUES (5, 'Земельно-имущественные отношения
+');
+INSERT INTO "spher" VALUES (6, 'Информационные технологии и связь
+');
+INSERT INTO "spher" VALUES (7, 'Картография, гиодезия и гидрометеорология
+');
+INSERT INTO "spher" VALUES (8, 'Культура и искусство
+');
+INSERT INTO "spher" VALUES (9, 'Налоги и сборы
+');
+INSERT INTO "spher" VALUES (10, 'Некоммерческие организации');
+INSERT INTO "spher" VALUES (11, 'Образование и наука
+');
+INSERT INTO "spher" VALUES (12, 'Предпринимательская деятельность
+');
+INSERT INTO "spher" VALUES (13, 'Природопользование и экология
+');
+INSERT INTO "spher" VALUES (14, 'Производство, строительство и торговля
+');
+INSERT INTO "spher" VALUES (15, 'Прочие услуги
+');
+INSERT INTO "spher" VALUES (16, 'Реклама и СМИ
+');
+INSERT INTO "spher" VALUES (17, 'Социальное обеспечение
+');
+INSERT INTO "spher" VALUES (18, 'Страхование
+');
+INSERT INTO "spher" VALUES (19, 'Таможенное дело
+');
+INSERT INTO "spher" VALUES (20, 'Транспорт и дорожное хозяйство
+');
+INSERT INTO "spher" VALUES (21, 'Экономика, финансы, статистика
+');
+INSERT INTO "spher" VALUES (22, 'Энергетика и природные ресурсы
+');
+INSERT INTO "spher" VALUES (23, 'Юридические услуги
+');
