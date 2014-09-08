f50354d64a3b99aa360e2c0e040dff73e6ead48e
Станислав <fomin.stanislav@gmail.com>
2014-09-09 02:10:04 +0400

    History logs view

--- application/models/history_log.php
+++ application/models/history_log.php
@@ -3,6 +3,8 @@
 class History_log extends APP_Model {
 
     public $_table = 'history_log';
+    public $primary_key = 'id_history_log';
+    protected $return_type = 'array';
 
     public function insert_log($history_log) {
         $history_log['id_user'] = $this->session->userdata('id');
@@ -11,4 +13,5 @@ class History_log extends APP_Model {
         $history_log['time'] = mdate($datestring, $time);
         $this->insert($history_log);
     }
+
 }
