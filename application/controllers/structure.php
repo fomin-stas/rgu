03e28c75b36f9f556a45f21ebf1849ff9b4e19c0
fomin-stas <fomin.stanislav@gmail.com>
2014-11-06 13:05:24 +0400

    Если шапки таблицы
    Если короткого наименования нет - выводим полное наименование

--- application/controllers/structure.php
+++ application/controllers/structure.php
@@ -64,7 +64,7 @@ class Structure extends APP_Controller {
 
         foreach ((array) $properties as $property) {
             $property['code'] = (isset($property['code'])) ? $property['code'] : $property['id_property'] . '_code';
-            $column_names[] = $property['property_short_name'];
+            $column_names[] = $property['property_short_name']=='' || is_null($property['property_short_name'])?$property['property_name']:$property['property_short_name'];
             $model['name'] = $property['code'];
             $model['index'] = $property['code'];
             switch ($property['format']['property_format_name']) {
