60ee6df0b6e0749ced38f88a821ca712c73f1794
Станислав <fomin.stanislav@gmail.com>
2014-12-18 08:47:14 +0300

    Update service.php

--- application/models/service.php
+++ application/models/service.php
@@ -4,4 +4,9 @@ class Service extends APP_Model {
 	public $_table = 'service';
 	public $primary_key = 'id_service';
 	protected $return_type = 'array';
+Public function get_services_by_authorities($authorities){
+    If(!is_array($authorities)){
+        $where='id_authority = '.$authorities;
+    }
+}
 }
